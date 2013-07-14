b670fd55f172ea03bdca7fa694263a811ebf8e72
Jason Ardell <ardell@gmail.com>
2013-07-14 19:50:30 -0400

    Install rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atljs::Application.config.secret_token = 'a4d0bb8c5e2558676868ed8e6f9784792443b08dbe50ea793144e3b8ba0fc0ea07e26e4c175a564da513330f5a2f12e2f6b30f18349e57dfac35f5b2108c3505'
